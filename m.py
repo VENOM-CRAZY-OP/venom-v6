b13433e089c315ae5df1bda8514c067e6f3f89a1
Ghatak11 <nikhilsing046@gmail.com>
2024-09-22 16:17:15 +0530

    Update m.py

--- m.py
+++ m.py
@@ -48,7 +48,7 @@ allowed_user_ids = read_users()
 
 # Function to log command to the file
 def log_command(user_id, target, port, time):
-    admin_id = ["5588464519"]
+    admin_id = ["6224024549"]
     user_info = bot.get_chat(user_id)
     if user_info.username:
         username = "@" + user_info.username
